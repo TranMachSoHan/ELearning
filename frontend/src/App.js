9744c9337c895a0282911f050b7c323be0f02d53
Hoang Minh Quan <64230078+minhquan1701@users.noreply.github.com>
2022-12-11 21:24:20 +0700

    course list by skill page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 
 import { Route, Routes } from 'react-router-dom';
+import CourseBySkill from './pages/CourseBySkill';
 
 import Home from './pages/Home';
 
@@ -8,6 +9,7 @@ function App() {
     <Routes>
 
         <Route path='/' element={<Home/>} ></Route>
+        <Route path='/skill' element={<CourseBySkill/>} ></Route>
 
       
     </Routes>
