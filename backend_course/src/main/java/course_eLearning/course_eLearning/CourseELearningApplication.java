85ed6309285eff44a322994b64e53223a973c934
khang <khangnick14@gmail.com>
2023-01-02 15:24:46 +0700

    2/1/2023

--- backend_course/src/main/java/course_eLearning/course_eLearning/CourseELearningApplication.java
+++ backend_course/src/main/java/course_eLearning/course_eLearning/CourseELearningApplication.java
@@ -14,12 +14,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class CourseELearningApplication implements CommandLineRunner {
 
 	@Autowired
--- backend_user_details/template-sample/src/main/java/com/example/templatesample/TemplateSampleApplication.java
+++ backend_user_details/template-sample/src/main/java/com/example/templatesample/TemplateSampleApplication.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -25,6 +27,7 @@ import java.util.Arrays;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class TemplateSampleApplication implements CommandLineRunner {
 	@Autowired
 	private ProfileRepository profileRepository;
