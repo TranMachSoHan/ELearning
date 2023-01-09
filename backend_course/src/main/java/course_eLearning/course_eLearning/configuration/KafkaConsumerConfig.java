042b2dd5cc581416c7c1603e5820a2881b4f54a8
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2023-01-09 17:38:18 +0700

    INTEGRATE kafka to course backend (#27)

--- backend_course/src/main/java/course_eLearning/course_eLearning/configuration/KafkaConsumerConfig.java
+++ backend_course/src/main/java/course_eLearning/course_eLearning/configuration/KafkaConsumerConfig.java
@@ -12,6 +12,11 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 import course_eLearning.course_eLearning.model.Comment;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import static course_eLearning.course_eLearning.constants.KafkaConstants.GROUP_ID;
+import static course_eLearning.course_eLearning.constants.KafkaConstants.KAFKA_BROKER;
 
 @EnableKafka
 @Configuration
@@ -19,10 +24,13 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, Comment> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKER);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, JsonDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
