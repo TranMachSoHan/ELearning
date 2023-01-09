39e515a3335c007632098707dd9321ed0173f90c
khang <khangnick14@gmail.com>
2023-01-09 15:33:47 +0700

    delete un file

--- backend_user_details/template-sample/src/main/java/com/example/templatesample/repository/FileMetaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.templatesample.repository;
-
-import com.example.templatesample.model.FileMeta;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FileMetaRepository extends MongoRepository<FileMeta, String> {
-}
