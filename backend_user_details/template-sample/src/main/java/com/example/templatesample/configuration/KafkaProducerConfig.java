547cf4a8ac1204b4d35b5f7df5cf5c8ffd9d389e
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2023-01-10 12:40:24 +0700

    KAFKA BROKER CONTAINER

--- backend_user_details/template-sample/src/main/java/com/example/templatesample/configuration/KafkaProducerConfig.java
+++ backend_user_details/template-sample/src/main/java/com/example/templatesample/configuration/KafkaProducerConfig.java
@@ -15,7 +15,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "e_learning_kafka1_1:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
